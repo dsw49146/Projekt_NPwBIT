ba9d9b0fc548094813a2b090e1bc36e6be46c5ad
Marcin <dsw49146@student.dsw.edu.pl>
2023-06-07 15:02:24 +0200

    Task7

--- converter.py
+++ converter.py
@@ -50,3 +50,36 @@ if format == "json":
 elif format == "yml" or format == "yaml":
     with open(output_file, 'w') as y:
         yaml.dump(data, y, default_flow_style=False)
+
+elif format == "xml":
+    try:
+        def dict_to_xml(data, root):
+            if isinstance(data, dict):
+                for key, value in data.items():
+                    child = ET.SubElement(root, key)
+                    dict_to_xml(value, child)
+            elif isinstance(data, list):
+                for item in data:
+                    dict_to_xml(item, root)
+            else:
+                root.text = str(data)
+
+        root = ET.Element('data')
+        dict_to_xml(data, root)
+        tree = ET.ElementTree(root)
+        xml_string = ET.tostring(root, encoding='utf-8')
+        dom = xml.dom.minidom.parseString(xml_string)
+        formatted_xml = dom.toprettyxml(indent="  ")
+
+        with open(output_file, 'w') as f:
+            f.write(formatted_xml)
+
+    except Exception as e:
+        print(f'Błąd zapisu pliku XML: {e}')
+        exit(1)
+
+else:
+    print("Nieobsługiwany format pliku wyjściowego:", format)
+    exit(1)
+
+print("Konwersja udana")
