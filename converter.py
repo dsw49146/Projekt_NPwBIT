fdbc387c165b303009f0f9754a86965240dd1c59
Marcin <dsw49146@student.dsw.edu.pl>
2023-06-07 14:38:59 +0200

    Task2

--- converter.py
+++ converter.py
@@ -4,3 +4,18 @@ parser = argparse.ArgumentParser(description='Konwerter plików json, xml, yaml'
 parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
 parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
 parser.add_argument('--format', choices=['json', 'yaml', 'xml'], help='Format pliku wyjściowego')
+
+args = parser.parse_args()
+input_file = args.input_file
+output_file = args.output_file
+format = args.format
+
+if input_file.endswith('.json'):
+    with open(input_file, 'r') as j:
+        try:
+            data = json.load(j)
+        except json.JSONDecodeError as e:
+            print("Błąd w parsowaniu pliku JSON: ", e)
+            exit(1)
+
+# -----
